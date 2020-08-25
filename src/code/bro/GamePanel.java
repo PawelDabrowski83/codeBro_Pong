631c943902a0f109e59be563f4f03293a3d87448
fluor <pawel.d.dabrowski@gmail.com>
2020-08-25 15:09:57 +0200

    ball bounces off paddles, yet game is not ending

--- src/code/bro/GamePanel.java
+++ src/code/bro/GamePanel.java
@@ -36,7 +36,8 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void newBall() {
-
+        random = new Random();
+        ball = new Ball((GAME_WIDTH / 2) - (BALL_DIAMETER / 2), (GAME_HEIGHT / 2) - (BALL_DIAMETER / 2), BALL_DIAMETER, BALL_DIAMETER);
     }
 
     public void newPaddles() {
@@ -54,13 +55,48 @@ public class GamePanel extends JPanel implements Runnable {
     public void draw(Graphics g) {
         paddle1.draw(g);
         paddle2.draw(g);
+        ball.draw(g);
     }
 
     public void move() {
-
+        paddle1.move();
+        paddle2.move();
+        ball.move();
     }
 
     public void checkCollision() {
+        //bounce ball off top & bottom window edges
+        if (ball.y <= 0) {
+            ball.setYDirection(-ball.yVelocity);
+        }
+        if (ball.y >= GAME_HEIGHT - BALL_DIAMETER) {
+            ball.setYDirection(-ball.yVelocity);
+        }
+
+        //bounces ball off paddles
+        if (ball.intersects(paddle1)) {
+            ball.xVelocity = Math.abs(ball.xVelocity);
+            ball.xVelocity++; // speeds up ball
+            if (ball.yVelocity > 0) {
+                ball.yVelocity++; // speeds up ball
+            } else {
+                ball.yVelocity--;
+            }
+            ball.setXDirection(ball.xVelocity);
+            ball.setYDirection(ball.yVelocity);
+        }
+        if (ball.intersects(paddle2)) {
+            ball.xVelocity = Math.abs(ball.xVelocity);
+            ball.xVelocity++; // speeds up ball
+            if (ball.yVelocity > 0) {
+                ball.yVelocity++; // speeds up ball
+            } else {
+                ball.yVelocity--;
+            }
+            ball.setXDirection(-ball.xVelocity);
+            ball.setYDirection(ball.yVelocity);
+        }
+
         //stops paddles at window edges
         if (paddle1.y <= 0) {
             paddle1.y = 0;
