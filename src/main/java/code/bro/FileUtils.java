d7aa6fdaba20e84c763f8e46fb6c060787934fdc
fluor <pawel.d.dabrowski@gmail.com>
2020-08-26 10:49:39 +0200

    new feature - file utils to read and write highscore from file

---
+++ src/main/java/code/bro/FileUtils.java
@@ -0,0 +1,28 @@
+package code.bro;
+
+import java.io.*;
+
+public class FileUtils {
+
+    public static void copy(InputStream input, OutputStream output, int bufferSize)
+            throws IOException {
+        byte[] buf = new byte[bufferSize];
+        int bytesRead = input.read(buf);
+        while (bytesRead != -1) {
+            output.write(buf, 0, bytesRead);
+            bytesRead = input.read(buf);
+        }
+        output.flush();
+    }
+
+    public static byte[] readFile(File file) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        copy(new FileInputStream(file), baos, 1000);
+        return baos.toByteArray();
+    }
+
+    public static void writeFile(File file, byte[] bytes) throws IOException {
+        ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+        copy(bais, new FileOutputStream(file), 1000);
+    }
+}
